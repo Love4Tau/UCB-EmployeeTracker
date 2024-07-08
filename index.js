1298b72fcce4e8a000fb109178ebf2d50137a3df
Lovely Tautuaa <lovelytau650@gmail.com>
2024-07-08 14:30:35 -0700

    Added start script and created inquirer prompt to ask for user input

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const inquirer = require("inquirer");
-
-inquirer.prompt ([
-    {
-        type: `list`,
-        choices: [`view all departments`, `view all roles`, `view all employees`, `add a department`, `add a role`, `add an employee`, `update an employee role`],
-        name: `What would you like to do?`,
-    },
-])
+const mysql = require("mysql");
+function init() {
+    inquirer.prompt ([
+        {
+            type: `list`,
+            name: `action`,
+            message: `What would you like to do?`,
+            choices: [`view all departments`, `view all roles`, `view all employees`, `add a department`, `add a role`, `add an employee`, `update an employee role`]
+        },
+    ])
+}
+init();
